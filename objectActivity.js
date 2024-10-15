f87c735ae9ff93211f5d167b468131ae18dd3246
Jean Matbagan <matbaganjean@gmail.com>
2024-10-15 21:00:32 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,15 @@
+//Object called car with the properties type, model, and color
+let car = {
+        type:"mid-engine sports car", 
+        model:"Ferrari Daytona SP3",
+        color:"Rosso Corsa"
+};
+// Check the type of the object
+console.log(typeof car);
+
+//Update the type property
+car.type = "Toyota";
+console.log(car); 
+// Used to add new property
+car.wheels = 4;
+console.log(car);
